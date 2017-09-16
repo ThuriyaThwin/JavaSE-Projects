d8ae1f5484588c52f8dfafe291ab5c5691255c6e
minlwin <lwin.zawmin@gmail.com>
2017-09-16 22:08:53 +0630

    Update Input Style

--- iShop/src/com/jdc/ishop/controller/Home.java
+++ iShop/src/com/jdc/ishop/controller/Home.java
@@ -1,5 +1,34 @@
 package com.jdc.ishop.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class Home {
+
+    @FXML
+    private BarChart<?, ?> chart;
+
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private Label members;
+
+    @FXML
+    private Label categories;
+
+    @FXML
+    private Label items;
+
+    @FXML
+    void reset(ActionEvent event) {
+
+    }
 
 }
