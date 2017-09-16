1c395f1baa1edf85bf9d56558233065b758dd7f3
minlwin <lwin.zawmin@gmail.com>
2017-09-16 18:16:09 +0630

    Add Transition to switch views

--- iShop/src/com/jdc/ishop/controller/ManagerHome.java
+++ iShop/src/com/jdc/ishop/controller/ManagerHome.java
@@ -5,6 +5,10 @@ import java.util.ResourceBundle;
 
 import com.jdc.ishop.IShopException;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +17,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class ManagerHome implements Initializable {
 
@@ -67,14 +72,40 @@ public class ManagerHome implements Initializable {
 		try {
 			
 			// set title
-			this.title.setText(title);
+			FadeTransition fadeIn = new FadeTransition(Duration.millis(200), this.title);
+			fadeIn.setFromValue(1.0);
+			fadeIn.setToValue(0);
+			fadeIn.setAutoReverse(false);
+			fadeIn.setCycleCount(1);
+			
+			fadeIn.setOnFinished(event -> this.title.setText(title));
+			
+			FadeTransition fadeOut = new FadeTransition(Duration.millis(600), this.title);
+			fadeOut.setFromValue(0);
+			fadeOut.setToValue(1.0);
+			fadeOut.setAutoReverse(false);
+			fadeOut.setCycleCount(1);
+			
+			SequentialTransition titleTrans = new SequentialTransition(fadeIn, fadeOut);
+			titleTrans.play();
 			
 			// load view
 			Parent view = FXMLLoader.load(getClass().getResource(viewName));
-			
-			// set to context view
+			view.setLayoutX(1180);
 			contextView.getChildren().clear();
 			contextView.getChildren().add(view);
+			
+			// set to context view
+			TranslateTransition trans = new TranslateTransition(Duration.millis(800), view);
+			trans.setByX(1180);
+			trans.setFromX(1180);
+			trans.setToX(0);
+			
+			trans.setAutoReverse(false);
+			trans.setCycleCount(1);
+			
+			ParallelTransition move = new ParallelTransition(titleTrans, trans);
+			move.play();
 			
 		} catch (IShopException e) {
 			message.setText(e.getMessage());
