f74dd575acdb0ae03eacdea1bab3180a2541fd1d
minlwin <minlwin@192.168.1.3>
2017-12-02 15:26:47 +0630

    Add Views

---
+++ hotel-application/src/com/jdc/hotel/application/view/AdminHome.java
@@ -0,0 +1,22 @@
+package com.jdc.hotel.application.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.HBox;
+
+public class AdminHome {
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private HBox buttonBar;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label message;
+
+}
