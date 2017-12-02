d116db380574fed3c033a865bbb5cbd4f2fa6b40
minlwin <minlwin@192.168.1.3>
2017-12-02 15:57:21 +0630

    Share Menu Operation

--- hotel-application/src/com/jdc/hotel/application/view/AdminHome.java
+++ hotel-application/src/com/jdc/hotel/application/view/AdminHome.java
@@ -1,11 +1,19 @@
 package com.jdc.hotel.application.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
-public class AdminHome {
+public class AdminHome implements Initializable{
 
     @FXML
     private MenuBar menuBar;
@@ -18,5 +26,38 @@ public class AdminHome {
 
     @FXML
     private Label message;
+    
+    @FXML
+    private StackPane content;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		message.setText("");
+		title.setText("Hotel Manager Home");
+		
+		menuBar.getMenus().stream().flatMap(menu -> menu.getItems().stream())
+			.filter(item -> !(item instanceof SeparatorMenuItem)).forEach(item -> {
+				item.setOnAction(event -> {
+					if(item.getText().equals("Close")) {
+						title.getScene().getWindow().hide();
+					} else if (item.getText().equals("Change Password")) {
+						// show pop up
+					} else {
+						try {
+							title.setText(item.getText());
+							String view = item.getText().replaceAll(" ", "").concat(".fxml");
+							Parent root = FXMLLoader.load(getClass().getResource(view));
+							content.getChildren().clear();
+							content.getChildren().add(root);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+						
+					}
+				});
+			});
+		
+	}
 
 }
--- hotel-application/src/com/jdc/hotel/application/view/Login.java
+++ hotel-application/src/com/jdc/hotel/application/view/Login.java
@@ -1,31 +1,48 @@
 package com.jdc.hotel.application.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Login {
 
-    @FXML
-    private Label message;
+	@FXML
+	private Label message;
 
-    @FXML
-    private TextField loginId;
+	@FXML
+	private TextField loginId;
 
-    @FXML
-    private PasswordField password;
-    
-    public void close() {
-    		message.getScene().getWindow().hide();
-    }
-    
-    public void managerLogin() {
-    	
-    }
-    
-    public void receptionLogin() {
-    	
-    }
+	@FXML
+	private PasswordField password;
+
+	public void close() {
+		message.getScene().getWindow().hide();
+	}
+
+	public void managerLogin() {
+		try {
+			// login operation
+			
+			// show manager view
+			Stage stage = new Stage(StageStyle.UNDECORATED);
+			Parent root = FXMLLoader.load(getClass().getResource("AdminHome.fxml"));
+			stage.setScene(new Scene(root));
+			stage.show();
+			
+			close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void receptionLogin() {
+
+	}
 
 }
