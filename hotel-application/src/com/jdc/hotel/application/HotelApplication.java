570d6c39a7cee49cdf4270f1aaaf10d5813f6834
minlwin <minlwin@192.168.1.3>
2017-12-02 14:54:04 +0630

    Commit Login View

--- hotel-application/src/com/jdc/hotel/application/HotelApplication.java
+++ hotel-application/src/com/jdc/hotel/application/HotelApplication.java
@@ -1,14 +1,23 @@
 package com.jdc.hotel.application;
 
+import com.jdc.hotel.application.view.Login;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class HotelApplication extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader.load(Login.class.getResource("Login.fxml"));
+		stage.setScene(new Scene(root));
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.show();
 	}
 
 	public static void main(String[] args) {
--- hotel-application/src/com/jdc/hotel/application/view/Login.java
+++ hotel-application/src/com/jdc/hotel/application/view/Login.java
@@ -1,5 +1,31 @@
 package com.jdc.hotel.application.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Login {
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private TextField loginId;
+
+    @FXML
+    private PasswordField password;
+    
+    public void close() {
+    		message.getScene().getWindow().hide();
+    }
+    
+    public void managerLogin() {
+    	
+    }
+    
+    public void receptionLogin() {
+    	
+    }
 
 }
