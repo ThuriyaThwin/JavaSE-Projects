7b183f532fef53a306692260f1af2a266f1ec99a
minlwin <minlwin@192.168.1.6>
2017-12-10 15:46:13 +0630

    Commit Resources

---
+++ smart-hotel/smart-hotel-ejb/src/main/resources/META-INF/init.sql
@@ -0,0 +1,9 @@
+insert into MasterData (tblName, colName, value, delFlag) values ('Customer', 'nationality', 'Myanmar', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Customer', 'nationality', 'Chinese', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Customer', 'nationality', 'India', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Customer', 'nationality', 'Japanese', false);
+
+insert into MasterData (tblName, colName, value, delFlag) values ('Reservation', 'status', 'Reserved', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Reservation', 'status', 'Check In', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Reservation', 'status', 'Check Out', false);
+insert into MasterData (tblName, colName, value, delFlag) values ('Reservation', 'status', 'Cancel', false);
