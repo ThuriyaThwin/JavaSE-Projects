3d163ae71a8e076b9c2b304bd0c932f80e8fbf42
minlwin <minlwin@192.168.1.4>
2018-01-16 15:14:21 +0630

    Commit Category Views

--- jdbc1-app/src/com/jdc/se/shop/views/CategoryManagement.java
+++ jdbc1-app/src/com/jdc/se/shop/views/CategoryManagement.java
@@ -1,5 +1,32 @@
 package com.jdc.se.shop.views;
 
-public class CategoryManagement {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+
+public class CategoryManagement implements Initializable{
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private TextField name;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addNew() {
+		
+	}
+	
+	public void upload() {
+		
+	}
 }
